66b889ec7bf76863657eb83f4f56c729644af24d
Mitali Kumari <113256176+mitalikumari10@users.noreply.github.com>
2023-10-04 21:42:38 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,37 @@
+//day name of week 
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter number 1 to 7"<<endl;
+	cin>>n;
+	if (n==1)
+	{
+		cout<<"sun";
+	}
+	else if(n==2)
+	{
+		cout<<"mon";
+	}
+	else if(n==3)
+	{
+		cout<<"tues";
+	}
+	else if(n==4)
+	{
+		cout<<"wed";
+	}
+	else if(n==5)
+	{
+		cout<<"thu";
+}
+    else if(n==6)
+	{
+		cout<<"fri";  
+	}
+	else if(n==7)
+	{
+		cout<<"sat";
+	}
+}
