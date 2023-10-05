d9257cef64436138f857ede61a1a9f9db3f950cc
Mitali Kumari <113256176+mitalikumari10@users.noreply.github.com>
2023-10-05 22:38:32 +0530

    Add files via upload

---
+++ notes.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int amount;
+
+    int note1,note2, note5,note10, note20, note50,note100,note200,note500;
+
+    note1 = note2 = note5 = note10=note20= note50 = note100 = note500 =note200=0;
+
+    cout << "Enter amount: "<<endl;
+    cin >> amount;
+    if(amount >= 500)//2200
+    {
+        note500 = amount/500;//kitne note honge 500 ke
+        amount -= note500 * 500;//update
+    }
+    if(amount >= 200)
+    {
+        note200 = amount/200;
+        amount -= note200 * 200;
+    }
+    if(amount >= 100)
+    {
+        note100 = amount/100;
+        amount -= note100 * 100;
+    }
+    if(amount >= 50)
+    {
+        note50 = amount/50;
+        amount -= note50 * 50;
+    }
+    if(amount >= 20)
+    {
+        note20 = amount/20;
+        amount -= note20 * 20;
+    }
+    if(amount >= 10)
+    {
+        note10 = amount/10;
+        amount -= note10 * 10;
+    }
+    if(amount >= 5)
+    {
+        note5 = amount/5;
+        amount -= note5 * 5;
+    }
+    if(amount >= 2)
+    {
+        note2 = amount /2;
+        amount -= note2 * 2;
+    }
+    if(amount >= 1)
+    {
+        note1 = amount;
+    }
+
+    cout << "Total number of notes" <<endl;
+    cout << "500 = " << note500 <<endl;
+    cout << "200 = " << note200 <<endl;
+    cout << "100 = " << note100 <<endl;
+    cout << "50 = " << note50 <<endl;
+    cout << "20 = " << note20 <<endl;
+    cout << "10 = " << note10 <<endl;
+    cout << "5 = " << note5 <<endl;
+    cout << "2 = " << note2 <<endl;
+    cout << "1 = " << note1 <<endl;
+
+    return 0;
+
+}
