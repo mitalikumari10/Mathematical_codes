97c4986989f31befc699504ad3beace1e4e7d439
Mitali Kumari <113256176+mitalikumari10@users.noreply.github.com>
2023-10-07 20:35:41 +0530

    Add files via upload

---
+++ sumevennos.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,i,even,odd,sum=0;
+    cout<<"enter the max number:"<<endl;
+    cin>>num;
+    for(i=1;i<=num;i++);
+     {
+      if (i%2==0)
+      {
+        cout<<even;
+      }
+      else
+      {
+        cout<<odd;
+      }
+      sum=sum+odd;
+     }
+      cout<<sum;
+      
+    
+}
